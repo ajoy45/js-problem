f4aa55b65b4d730cfc3e723eae6f9bf0f4fc8186
ajoy45 <ajoypust@gmail.com>
2023-07-26 09:18:51 +0600

    task1 complete

--- solve.js
+++ solve.js
@@ -1,1 +1,12 @@
 // Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+const reverseString=(str)=>{
+    const part=str.split('');
+    let arr=[]
+    for(let i=part.length-1;i>=0;i--){
+        const char=part[i];
+        arr.push(char)
+    
+    }
+    console.log(arr.join(''))
+}
+reverseString("i am ajoy biswas now i want to be a developer")
