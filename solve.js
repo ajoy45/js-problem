0ba9744d9aaba954656047f74353ea70c87280a0
ajoy45 <ajoypust@gmail.com>
2023-07-27 09:46:57 +0600

    task7

--- solve.js
+++ solve.js
@@ -114,4 +114,36 @@ function mostFrequentElement(array) {
   }
   const password=generatePassword(12)
   console.log(password);
+// Task 7: Implement a function that converts a Roman numeral to an integer. The function should take a Roman numeral string (e.g., "IX" or "XXI") as input and return the corresponding integer value.
+  function romanToInteger(roman) {
+    const romanNumerals = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    };
+  
+    let result = 0;
+  
+    for (let i = 0; i < roman.length; i++) {
+      const currentNumeral = romanNumerals[roman[i]];
+      const nextNumeral = romanNumerals[roman[i + 1]];
+      console.log(currentNumeral,nextNumeral)
+  
+      if (nextNumeral > currentNumeral) {
+        result += nextNumeral - currentNumeral;
+        i++; // Skip the next numeral as it has already been accounted for.
+      } else {
+        result += currentNumeral;
+      }
+    }
+  
+    return result;
+  }
+  
+ 
+  console.log(romanToInteger("XX"));
   
