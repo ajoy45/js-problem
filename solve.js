d78ab6bbf57e2196bba9e59e69c6b403421b17e0
ajoy45 <ajoypust@gmail.com>
2023-07-29 09:56:37 +0600

    task11

--- solve.js
+++ solve.js
@@ -184,3 +184,17 @@ function mostFrequentElement(array) {
   }
   const result4=checkOddAndEven(0);
   console.log(result4)
+  // task11.JavaScript Program to Find the Largest Among Three Numbers
+  const largestNumber=(num1,num2,num3)=>{
+        if(num1>num2&&num1>num3){
+          return num1
+        }
+        else if(num2>num1&&num2>num3){
+          return mum2
+        }
+        else{
+          return num3
+        }
+  }
+  const result5=largestNumber(5,3,4);
+  console.log(result5)
